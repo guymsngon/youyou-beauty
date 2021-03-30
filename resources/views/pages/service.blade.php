73e4f46da5459c8a0b9ce1c211a462c0dc5154c4
denisnyemb <63614309+denisnyemb@users.noreply.github.com>
2021-03-30 22:35:15 +0200

    denis update

--- resources/views/pages/service.blade.php
+++ resources/views/pages/service.blade.php
@@ -28,19 +28,19 @@
 			<div class="section-full content-inner-2 bg-white hair-services">
 				<div class="container">
 					<div class="section-head text-black text-center">
-						<h2 class="text-primary m-b10">Our Services</h2>
+						<h2 class="text-primary m-b10">Services</h2>
 						<div class="dlab-separator-outer m-b0">
 							<div class="dlab-separator text-primary style-icon"><i class="flaticon-spa text-primary"></i></div>
 						</div>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+						<p>Youyou Beauty vous offre les meilleurs services en matière de santé, beauté, coiffure,soins faciaux,esthétiques et bein d'autres...</p>
 					</div>
 					<div class="row">
 						<div class="col-lg-4 col-md-6 col-sm-6 p-lr0">
 							<div class="icon-bx-wraper center p-a30">
 								<div class="icon-lg radius m-b20"> <a href="#" class="icon-cell"><i class="flaticon-barbershop"></i></a> </div>
 								<div class="icon-content">
-									<h5 class="dez-tilte"><a href="#">Haircut & Styling</a></h5>
-									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod [...]</p>
+									<h5 class="dez-tilte"><a href="#">Coifuure et Style</a></h5>
+									<p>les coiffueuses de Youyou Beauty sont des professionnelles , qualifiées et formées qui sont toujours à l'attente de vos recommandations et suggestions. </p>
 								</div>
 							</div>
 						</div>
@@ -49,7 +49,7 @@
 								<div class="icon-lg radius m-b20"> <a href="#" class="icon-cell"><i class="flaticon-makeup"></i></a> </div>
 								<div class="icon-content">
 									<h5 class="dez-tilte"><a href="#">Makeup</a></h5>
-									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod [...]</p>
+									<p>Voulez-vous aller à un rendez-vous galant ou à une soirée de Galla? ne vous inquiétez plus. Les femmes Youyou Beaty est là pour vous Transformer avec ses différents Makeup à savoir: Maquillage du jour, Maquillage du Soir , face painting et Face Painting.  </p>
 								</div>
 							</div>
 						</div>
@@ -58,7 +58,7 @@
 								<div class="icon-lg radius m-b20"> <a href="#" class="icon-cell"><i class="flaticon-makeup-1"></i></a> </div>
 								<div class="icon-content">
 									<h5 class="dez-tilte"><a href="#">Manicure & Pedicure</a></h5>
-									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod [...]</p>
+									<p>Les salons de beauté Youyou Beauty vos offrent les meilleurs idées pour vos ongles [...]</p>
 								</div>
 							</div>
 						</div>
@@ -66,8 +66,8 @@
 							<div class="icon-bx-wraper center p-a30">
 								<div class="icon-lg radius m-b20"> <a href="#" class="icon-cell"><i class="flaticon-woman-1"></i></a> </div>
 								<div class="icon-content">
-									<h5 class="dez-tilte"><a href="#">Skin Care</a></h5>
-									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod [...]</p>
+									<h5 class="dez-tilte"><a href="#">Soins de la Peau</a></h5>
+									<p>Nous offrons les meilleurs services en ce qui est des soins de la peau comme les soins de visage, l'épilation etc. [...]</p>
 								</div>
 							</div>
 						</div>
@@ -75,8 +75,8 @@
 							<div class="icon-bx-wraper center p-a30">
 								<div class="icon-lg radius m-b20"> <a href="#" class="icon-cell"><i class="flaticon-woman"></i></a> </div>
 								<div class="icon-content">
-									<h5 class="dez-tilte"><a href="#">Body Treatment</a></h5>
-									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod [...]</p>
+									<h5 class="dez-tilte"><a href="#">Soins Corporels</a></h5>
+									<p>Hey!!! fini les problemes de peau et de laits corporels qui ne s'adaptent pas à vos exsigeances Youyou Beauty a tout ce dont il vous faut afin que vous retrouviez la joie.  [...]</p>
 								</div>
 							</div>
 						</div>
@@ -85,7 +85,7 @@
 								<div class="icon-lg radius m-b20"> <a href="#" class="icon-cell"><i class="flaticon-candle-1"></i></a> </div>
 								<div class="icon-content">
 									<h5 class="dez-tilte"><a href="#">Massage</a></h5>
-									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod [...]</p>
+									<p>Fini les douleurs corporelles Youyou Beauty vous offre le meilleur services de massage qu'il soit.[...]</p>
 								</div>
 							</div>
 						</div>
@@ -100,28 +100,8 @@
 							<div class="icon-bx-wraper p-lr15 p-b30 p-t20 bg-white center fly-box-ho">
 								<div class="icon-lg m-b10"> <span class="icon-cell text-primary"><i class="flaticon-woman"></i></span> </div>
 								<div class="icon-content">
-									<h6 class="dlab-tilte">We are Professional</h6>
-									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-									<a href="#" class="site-button-secondry">Site Button</a>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-6 col-sm-6 m-b30">
-							<div class="icon-bx-wraper p-lr15 p-b30 p-t20 bg-white center fly-box-ho">
-								<div class="icon-lg m-b10"><span class="icon-cell text-primary"><i class="flaticon-mortar"></i></span> </div>
-								<div class="icon-content">
-									<h6 class="dlab-tilte">Lux Cosmetic</h6>
-									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-									<a href="#" class="site-button-secondry">Site Button</a>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-3 col-md-6 col-sm-6 m-b30">
-							<div class="icon-bx-wraper p-lr15 p-b30 p-t20 bg-white center fly-box-ho">
-								<div class="icon-lg m-b10"> <span class="icon-cell text-primary"><i class="flaticon-candle"></i></span> </div>
-								<div class="icon-content">
-									<h6 class="dlab-tilte">Medical Education</h6>
-									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+									<h6 class="dlab-tilte">Nos sommes professionnels </h6>
+									<p>Youyou Beauty est l'un des meilleurs salons qui excelle dans le professionalisme.</p>
 									<a href="#" class="site-button-secondry">Site Button</a>
 								</div>
 							</div>
@@ -130,8 +110,8 @@
 							<div class="icon-bx-wraper p-lr15 p-b30 p-t20 bg-white center fly-box-ho">
 								<div class="icon-lg m-b10"> <span class="icon-cell text-primary"><i class="flaticon-sauna-1"></i></span> </div>
 								<div class="icon-content">
-									<h6 class="dlab-tilte">The Newest Equipment</h6>
-									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+									<h6 class="dlab-tilte">Nouvel Equipements </h6>
+									<p>Ne vous inquiétez plus Youyou Beauty dispose déjà en son sein des  équipements de beauté dans l'optique de satifaire sa clientèle pour le mieux.</p>
 									<a href="#" class="site-button-secondry">Site Button</a>
 								</div>
 							</div>
@@ -143,55 +123,55 @@
 			<div class="section-full content-inner-2 overlay-white-middle" style="background-image:url({{asset('images/background/bg1.png')}}), url({{asset('images/background/bg2.png')}}); background-position: bottom, top; background-size: 100%; background-repeat: no-repeat;">
 				<div class="container">
 					<div class="section-head text-black text-center">
-						<h2 class="text-primary m-b10">Our Professional Team</h2>
+						<h2 class="text-primary m-b10">Notre équipe de professionnels</h2>
 						<div class="dlab-separator-outer m-b0">
 							<div class="dlab-separator text-primary style-icon"><i class="flaticon-spa text-primary"></i></div>
 						</div>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+						<p>Youyou Beauty met à votre disposition une équipe de professionnels 7 jours/7 pour votre Bien-être.</p>
 					</div>
 					<div class="team-carousel owl-carousel owl-carousel owl-btn-center-lr owl-btn-3 owl-theme owl-dots-primary-full owl-loaded owl-drag">
 						<div class="item">
 							<div class="dlab-box text-center team-box">
-								<div class="dlab-media"> <img src="{{asset('images/our-team/pic1.jpg')}}" alt=""></div>
+								<div class="dlab-media"> <img src="{{asset('images/our-team/coiffeuse1.jpeg')}}" alt=""></div>
 								<div class="dlab-title-bx p-a10">
-									<h5 class="text-black m-a0"><a href="#">John Doe</a></h5>
-									<span class="clearfix">Cosmetologist</span>
+									<h5 class="text-black m-a0"><a href="#">Coiffeuse</a></h5>
+									<span class="clearfix">Coiffeuse</span>
 								</div>
 							</div>
 						</div>
 						<div class="item">
 							<div class="dlab-box text-center team-box">
-								<div class="dlab-media"> <img src="{{asset('images/our-team/pic2.jpg')}}" alt=""></div>
+								<div class="dlab-media"> <img src="{{asset('images/our-team/coiffeuse2.jpeg')}}" alt=""></div>
 								<div class="dlab-title-bx p-a10">
-									<h5 class="text-black m-a0"><a href="#">John Doe</a></h5>
-									<span class="clearfix">Cosmetologist</span>
+									<h5 class="text-black m-a0"><a href="#">Coiffeuse</a></h5>
+									<span class="clearfix">Coiffeuse</span>
 								</div>
 							</div>
 						</div>	
 						<div class="item">
 							<div class="dlab-box text-center team-box">
-								<div class="dlab-media"> <img src="{{asset('images/our-team/pic3.jpg')}}" alt=""></div>
+								<div class="dlab-media"> <img src="{{asset('images/our-team/esth1.jpeg')}}" alt=""></div>
 								<div class="dlab-title-bx p-a10">
-									<h5 class="text-black m-a0"><a href="#">John Doe</a></h5>
-									<span class="clearfix">Cosmetologist</span>
+									<h5 class="text-black m-a0"><a href="#">Esthéticienne</a></h5>
+									<span class="clearfix">Esthéticienne</span>
 								</div>
 							</div>
 						</div>	
 						<div class="item">
 							<div class="dlab-box text-center team-box">
-								<div class="dlab-media"> <img src="{{asset('images/our-team/pic5.jpg')}}" alt=""></div>
+								<div class="dlab-media"> <img src="{{asset('images/our-team/makuper1.jpeg')}}" alt=""></div>
 								<div class="dlab-title-bx p-a10">
-									<h5 class="text-black m-a0"><a href="#">John Doe</a></h5>
-									<span class="clearfix">Cosmetologist</span>
+									<h5 class="text-black m-a0"><a href="#">Makeuper</a></h5>
+									<span class="clearfix">Makeuper</span>
 								</div>
 							</div>
 						</div>	
 						<div class="item">
 							<div class="dlab-box text-center team-box">
-								<div class="dlab-media"> <img src="{{asset('images/our-team/pic4.jpg')}}" alt=""></div>
+								<div class="dlab-media"> <img src="{{asset('images/our-team/makeuper2.jpeg')}}" alt=""></div>
 								<div class="dlab-title-bx p-a10">
-									<h5 class="text-black m-a0"><a href="#">John Doe</a></h5>
-									<span class="clearfix">Cosmetologist</span>
+									<h5 class="text-black m-a0"><a href="#">Makeuper</a></h5>
+									<span class="clearfix">Makeuper</span>
 								</div>
 							</div>
 						</div>	
@@ -203,7 +183,7 @@
                 <div class="container">
 					<div class="row">
 						<div class="col-lg-12 col-md-12 text-white text-center">
-							<h2>Video Presentation </h2>
+							<h2>Video  Presentation </h2>
 							<p class="max-w700 m-auto">In this video, our staff members tell about their work at Solari, how they achieve the best results for their clients every day and more. Click the Play button below to watch this presentation.</p>
 							<div class="video-play-icon m-t50">
 								<a href="https://www.youtube.com/watch?v=rICXf0UH7Cg" class="popup-youtube video"><i class="ti-control-play"></i></a>
@@ -229,13 +209,13 @@
 								<div class="dlab-post-media dlab-img-effect "> <a href="#"><img src="{{asset('images/blog/grid/pic1.jpg')}}" alt=""></a> </div>
 								<div class="dlab-info p-t10">
 									<div class="dlab-post-title ">
-										<h5 class="post-title font-weight-500 m-t0"><a href="#">Latest product, full of marvelous features and functionality</a></h5>
+										<h5 class="post-title font-weight-500 m-t0"><a href="#">Dernier produit, plein de fonctionnalités et de fonctionnalités merveilleuses</a></h5>
 									</div>
 									<div class="dlab-post-text">
 									   <p>All the Lorem Ipsum generators on the Internet tend to repeat..</p>
 									</div>
 								   <div class="dlab-post-readmore"> 
-										<a href="#" title="READ MORE" rel="bookmark" class="site-button-link black outline">READ MORE <i class="ti-arrow-right"></i></a> 
+										<a href="#" title="READ MORE" rel="bookmark" class="site-button-link black outline">LIRE PLUS  <i class="ti-arrow-right"></i></a> 
 									</div>
 								</div>
 							</div>
@@ -245,13 +225,13 @@
 								<div class="dlab-post-media dlab-img-effect "> <a href="#"><img src="{{asset('images/blog/grid/pic2.jpg')}}" alt=""></a> </div>
 								<div class="dlab-info p-t10">
 									<div class="dlab-post-title ">
-										<h5 class="post-title font-weight-500 m-t0"><a href="#">Design a perfect website according to your need and desire</a></h5>
+										<h5 class="post-title font-weight-500 m-t0"><a href="#">Concevez un site Web parfait selon vos besoins et vos envies</a></h5>
 									</div>
 									<div class="dlab-post-text">
 									   <p>All the Lorem Ipsum generators on the Internet tend to repeat..</p>
 									</div>
 								   <div class="dlab-post-readmore"> 
-										<a href="#" title="READ MORE" rel="bookmark" class="site-button-link black outline">READ MORE <i class="ti-arrow-right"></i></a> 
+										<a href="#" title="READ MORE" rel="bookmark" class="site-button-link black outline">LIRE PLUS  <i class="ti-arrow-right"></i></a> 
 									</div>
 								</div>
 							</div>
